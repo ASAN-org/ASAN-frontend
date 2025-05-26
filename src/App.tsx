3b748dac14eba8e2d8145064aec9cf96ac0d122c
Arman <arman.zarkalam@gmail.com>
2025-05-26 15:36:58 +0330

    feat: header component initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import appRoutes from "./routes";
 import "./App.css";
 
 function App() {
-  const [siteTitle, setSiteTitle] = useState("Loading...");
+  //const [siteTitle, setSiteTitle] = useState("Loading...");
 
   useEffect(() => {
     fetch("https://asanorg.liara.run/webshop")
       .then((res) => res.json())
       .then((data) => {
-        setSiteTitle(data.title);
+        //setSiteTitle(data.title);
         console.log(data);
         document.title = data.title;
       });
   }, []);
 
-  return <>the title of webshop is {siteTitle}</>;
+  return (
+    <Router>
+      <Routes>{appRoutes}</Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,6 @@
+import { Route } from "react-router-dom";
+import Header from "./components/Header";
+
+const appRoutes = <Route path="/" element={<Header />}></Route>;
+
+export default appRoutes;
