5598415c1d26bd779fba33ce45b47667ed2be267
nazanin mahmoodi <nazaninm4646@gmail.com>
2025-06-03 20:21:08 +0330

    fix:deploy

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,5 @@
 import { Route } from "react-router-dom";
-import Header from "./components/Header";
-import { ImageSlider } from "./components/ImageSlider";
+
 import Homepage from "./pages/Homepage";
 
 const appRoutes = <Route path="/" element={<Homepage/>}></Route>;
